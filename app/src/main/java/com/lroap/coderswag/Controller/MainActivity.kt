7306e2e12451eb946993d2d68b0ba99c03907f9b
lauraalexandraroa <laualexandrar@gmail.com>
2019-11-18 20:16:49 -0800

    Lesson 2

--- app/src/main/java/com/lroap/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/lroap/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lroap.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.lroap.coderswag.Adapters.CategoryAdapter
 import com.lroap.coderswag.Model.Category
 import com.lroap.coderswag.R
 import com.lroap.coderswag.Services.DataService
@@ -10,15 +11,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-           // android.R.layout.simple_list_item_1, (estas son listas ya definidas por android, no las usaremos porque crearemos nuevas con adapter)
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
+
+//        adapter = ArrayAdapter(this,
+//           // android.R.layout.simple_list_item_1, (estas son listas ya definidas por android, no las usaremos porque crearemos nuevas con adapter)
+//            DataService.categories)
 
         categoryListView.adapter = adapter
     }
