29e39891957bb9bda07ad649016e0d074fe13af4
lauraalexandraroa <laualexandrar@gmail.com>
2019-11-18 21:07:01 -0800

    prepare for recycle view adapter

--- app/src/main/java/com/lroap/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/lroap/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,8 @@ package com.lroap.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import com.lroap.coderswag.Adapters.CategoryAdapter
 import com.lroap.coderswag.Model.Category
 import com.lroap.coderswag.R
@@ -24,5 +26,11 @@ class MainActivity : AppCompatActivity() {
 //            DataService.categories)
 
         categoryListView.adapter = adapter
+
+//        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+//            val category = DataService.categories[i]
+//            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+
+
     }
 }
