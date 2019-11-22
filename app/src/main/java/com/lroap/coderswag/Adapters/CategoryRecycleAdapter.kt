2531a7b0d871d0aec76eb6c519aba76a0fbf3968
lauraalexandraroa <laualexandrar@gmail.com>
2019-11-21 16:42:45 -0800

    Last version

---
+++ app/src/main/java/com/lroap/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -0,0 +1,4 @@
+package com.lroap.coderswag.Adapters
+
+class CategoryRecycleAdapter {
+}
---
+++ app/src/main/java/com/lroap/coderswag/Controller/ProductsActivity.kt
@@ -0,0 +1,12 @@
+package com.lroap.coderswag.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+    }
+}
